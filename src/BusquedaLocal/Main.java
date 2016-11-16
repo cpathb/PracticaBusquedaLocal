7e6309c09f069221dfa254b28ead5b4509d196a4
Carlos Parada Alfaya <carlos.p.a123@gmail.com>
2016-11-16 11:53:40 +0100

    Creado el proyecto
    Creado el proyecto con intelliJ

---
+++ src/BusquedaLocal/Main.java
@@ -0,0 +1,15 @@
+package BusquedaLocal;
+
+import java.util.*;
+
+public class Main {
+    static final int ciudades=10; // Definimos en esta constante el número de ciudades que habrá en la búsqueda
+
+
+
+    public static void main(String[] args) {
+
+        List<Integer> Sinicial= new ArrayList<Integer>();
+
+    }
+}
